7b9a9774ffa0394a2b71b4a15343e6ec62fce59b
minchen11 <amienishaq@gmail.com>
2022-09-12 03:17:16 +0700

    push pertama

---
+++ belajarLogin/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace belajarLogin
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtUsername = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtPassword = new System.Windows.Forms.TextBox();
+            this.CCBPassword = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 103);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // TxtUsername
+            // 
+            this.TxtUsername.Location = new System.Drawing.Point(230, 100);
+            this.TxtUsername.Name = "TxtUsername";
+            this.TxtUsername.Size = new System.Drawing.Size(228, 26);
+            this.TxtUsername.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(115, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // TxtPassword
+            // 
+            this.TxtPassword.Location = new System.Drawing.Point(230, 154);
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.Size = new System.Drawing.Size(228, 26);
+            this.TxtPassword.TabIndex = 1;
+            this.TxtPassword.TextChanged += new System.EventHandler(this.TxtPassword_TextChanged);
+            // 
+            // CCBPassword
+            // 
+            this.CCBPassword.AutoSize = true;
+            this.CCBPassword.Location = new System.Drawing.Point(310, 206);
+            this.CCBPassword.Name = "CCBPassword";
+            this.CCBPassword.Size = new System.Drawing.Size(148, 24);
+            this.CCBPassword.TabIndex = 2;
+            this.CCBPassword.Text = "Show Password";
+            this.CCBPassword.UseVisualStyleBackColor = true;
+            this.CCBPassword.CheckedChanged += new System.EventHandler(this.CCBPassword_CheckedChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(269, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 37);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Log in";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(597, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.CCBPassword);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtUsername);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtUsername;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtPassword;
+        private System.Windows.Forms.CheckBox CCBPassword;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ belajarLogin/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace belajarLogin
+{
+    public partial class Form1 : Form
+    {
+        readonly string Username = "admin";
+        readonly string Password = "123";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //fungsi agar textbox password jadi karakter bullet
+        private void TxtPassword_TextChanged(object sender, EventArgs e)
+        {
+            TxtPassword.UseSystemPasswordChar = true;
+        }
+
+
+        //fungsi agar saat checkbox (show password) diklik, kondisi textbox password yg awalnya bullet = true, menjadi false, dan menjadi karakter huruf lagi
+        private void CCBPassword_CheckedChanged(object sender, EventArgs e)
+        {
+            if (CCBPassword.Checked == true)
+            {
+                TxtPassword.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                TxtPassword.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (TxtPassword.Text == Password && TxtUsername.Text == Username)
+            {
+                MessageBox.Show("Kamu berhasil login");
+
+                //fungsi untuk pindah ke form berikutnya jika kondisi pertama true
+                Form2 a = new Form2();
+                a.Show();
+                this.Hide();
+            }
+            else if (TxtPassword.Text == "" || TxtUsername.Text == "")
+            {
+                MessageBox.Show("Kamu belum mengisi form login");
+            }
+            else
+            {
+                MessageBox.Show("Username atau Password Salah ");
+            }
+
+        }
+    }
+}
